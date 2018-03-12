1cb3a0f58de6960bad98c9f409812a73d6592f25
Matej Simek <matej@simek.pro>
2018-03-12 09:08:51 +0100

    better package.json

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const compression = require('compression')
-
-const app = express()
-
-app.use(compression())
-app.use(express.static('dist'))
-
-app.listen(process.env.PORT, () => console.log(`Express instance is listening on port ${process.env.PORT}`))
